0621e05efa6339193257ddf91245e81ff226d5e6
cel <celeyva91@gmail.com>
2022-08-29 22:01:45 -0400

    javaScript for robot's name

---
+++ assets/game.js
@@ -0,0 +1,15 @@
+//fight();
+
+function fight () {
+    window.alert("The fight has begun!");
+}
+
+var playerName = window.prompt("What is your robots name");
+
+console.log(playerName);
+
+console.log("This logs a string, good for leaving yourself a message");
+
+console.log(10+10);
+
+console.log("Our robot's name is"+ playerName);
