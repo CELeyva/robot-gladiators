ce9d345c2f6074f51ea4846142f8da313a8dd9dd
cel <celeyva91@gmail.com>
2022-09-12 00:30:01 -0400

    start and end game functions

--- assets/game.js
+++ assets/game.js
@@ -76,6 +76,12 @@ var fight = function(enemyName) {
 }
 };
 
+// Function to start a new game
+var startGame = function() {
+    // reset player stats
+    playerHealth = 100;
+    playerAttack = 10;
+    playerMoney = 10;
 
 for (var i = 0; i < enemyNames.length; i++) {
     if (playerHealth > 0) {
@@ -89,5 +95,36 @@ for (var i = 0; i < enemyNames.length; i++) {
     else {
         window.alert('You have lost your robot in battle! Game Over!');
         break;
+        }
     }
-}
+    endGame();
+};
+
+// End the game
+var endGame = function() {
+    
+    //if player is still alive, player wins!
+    if (playerHealth > 0) {
+        window.alert("Great Job, you've survived the game! You now have a acore of" + playerMoney + ".");
+    }
+
+    else {
+        window.alert("You've lost your robot in battle.");
+    }
+    
+
+    //ask player if they'd like to play again
+    var playAgainConfirm = window.confirm("Would you like to play again?");
+
+    if (playAgainConfirm) {
+        // restart the game
+        startGame();
+    }
+
+    else { 
+        window.alert("Thank you for playing Robot GLadiators! Come back soon!");
+    }
+};
+
+// Start the game when the page loads
+startGame();
