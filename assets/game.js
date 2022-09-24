24f50defd0ab32e0cd1ef450b061760980639bfb
cel <celeyva91@gmail.com>
2022-09-23 21:56:05 -0400

    fixed name bug

--- assets/game.js
+++ assets/game.js
@@ -1,26 +1,11 @@
-console.log(enemyInfo.names);
-console.log(enemyInfo.names.length);
-console.log(enemyInfo.names[0]);
-console.log(enemyInfo.names[3]);
+var randomNumber = function (min,max) {
+    var value = Math.floor(Math.random() * (max-min+1) + min);
 
-
-for(var i= 0; i < enemyInfo.names.length; i++) {
-    console.log(enemyInfo.names[i]);
-    console.log(i);
-    console.log(enemyInfo.names[i] + "is at" + i + "index");
+    return value;
 }
 
-// Game States
-
-// "WIN" - Player robot has defeated all enemy-robots
-// * Fight all enemy-robots
-// * Defeat all enemy-robots
-
-
-// "LOSE" - Player robot's health is zero or less
-
 var fight = function(enemy) {
-    while (playerInfo.health > 0 && enemyInfo.health > 0) {
+    while (playerInfo.health > 0 && enemy.health > 0) {
     var promptFight = window.prompt('Would you like to FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
 
     if (promptFight ==="skip"|| promptFight === "SKIP") {
@@ -37,27 +22,27 @@ var fight = function(enemy) {
 
     var damage= randomNumber(playerInfo.attack-3, playerInfo.attack);
 
-    enemyInfo.health = Math.max(0, enemyInfo.health - damage);
+    enemy.health = Math.max(0, enemy.health - damage);
     console.log(
-        playerInfo.name + ' attacked ' + enemyInfo.name + ' . ' + enemyInfo.name + ' now has ' + enemyInfo.health + ' health remaining'
+        playerInfo.name + ' attacked ' + enemy.name + ' . ' + enemy.name + ' now has ' + enemy.health + ' health remaining'
     )
 
-    if (enemyInfo.health <= 0) {
-        window.alert(enemyInfo.name + ' has died! ');
+    if (enemy.health <= 0) {
+        window.alert(enemy.name + ' has died! ');
 
         playerInfo.money = playerInfo.money + 20;
         break;
     } else; {
-        window.alert(enemyInfo.name + ' still has ' + enemyInfo.health + ' health left. ');
+        window.alert(enemy.name + ' still has ' + enemy.health + ' health left. ');
     }
 
 
 
-    var damage = randomNumber(enemyInfo.attack -3, enemyInfo.attack);
+    var damage = randomNumber(enemy.attack -3, enemy.attack);
 
     playerInfo.health = Math.max(0, playerInfo.health - damage);
     console.log( 
-        enemyInfo.name + ' has attacked ' + playerInfo.name + ' . ' + playerInfo.name + ' health remaining '
+        enemy.name + ' has attacked ' + playerInfo.name + ' . ' + playerInfo.name + ' health remaining '
     );
 
     if (playerInfo.health <= 0) {
@@ -73,15 +58,15 @@ var fight = function(enemy) {
 var startGame = function() {
     playerInfo.reset();
 
-for (var i = 0; i < enemyInfo.length; i++) {
+for (var i = 0; i < enemy.length; i++) {
     if (playerInfo.health > 0) {
         window.alert('Welcome to Robot Gladiators! Round '+ (i+1));
 
-        var pickedEnemyObj = enemyInfo[i];
+        var pickedEnemyObj = enemy[i];
         pickedEnemyObj.health = randomNumber(40,60);
         fight(pickedEnemyObj);
 
-        if (playerInfo.health > 0 && i < enemyInfo.name - 1) {
+        if (playerInfo.health > 0 && i < enemy.name - 1) {
             var storeConfirm = window.confirm("The fight is over, visit the store before the next round?");
             
             if (storeConfirm) {
@@ -133,7 +118,7 @@ switch(shopOptionPrompt) {
     case "REFILL": // new case
     case "refill":
         playerInfo.refillHealth();
-        break:
+        break;
 
     case "UPGRADE": // new case
     case "upgrade":
@@ -152,15 +137,20 @@ switch(shopOptionPrompt) {
 }
 };
 
+var getPlayerName = function() {
+    var name = "";
 
-var randomNumber = function(min,max) {
-    var value = Math.floor(Math.random() * (max - min +1 )+ min);
+    while (name === ""|| name === null){
+        name = prompt ("What is your robot's name?");
+    }
 
-    return value;
-};
+    console.log("Your robot's name is " + name);
+    return name;
+}
+
 
 var playerInfo = {
-    name: window.prompt("What is your robot's name?"),
+    name: getPlayerName(),
     health: 100,
     attack: 10,
     money: 10,
@@ -195,7 +185,7 @@ var playerInfo = {
 };
 
 
-var enemyInfo = [
+var enemy = [
     {
         name: "Roborto",
         attack: randomNumber(10,14)
